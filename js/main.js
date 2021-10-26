d324d658bc56767f902bfd01318859c1408caa72
Grigoriy_Belikov <grisha.belikov@gmail.com>
2021-10-26 18:04:08 +0300

    - Изменен способ записи контента на Document-Fragment.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 import {createArrayObject} from './data.js';
 import {createTemplate} from './paint-pictures.js';
 
-createArrayObject().forEach((comment) => createTemplate(comment));
+createTemplate(createArrayObject());
--- js/paint-pictures.js
+++ js/paint-pictures.js
@@ -1,4 +1,6 @@
 function createTemplate(objectComment) {
+  const fragment = document.createDocumentFragment();
+
   // Контейнер для комментариев
   const blockPastPictures = document.querySelector('.pictures');
 
@@ -6,20 +8,24 @@ function createTemplate(objectComment) {
   const contentPicture = document.querySelector('#picture').content;
   const elementPicture = contentPicture.querySelector('.picture');
 
-  // делаем копию шаблона
-  const infoPicturesTemplate = elementPicture.cloneNode(true);
+  // Пишем переданные значения
+  objectComment.forEach((comment) => {
+    // делаем копию шаблона
+    const infoPicturesTemplate = elementPicture.cloneNode(true);
 
-  // Находим в шаблоне picture нужные элементы
-  const imgPicture = infoPicturesTemplate.querySelector('.picture__img');
-  const commentsPicture = infoPicturesTemplate.querySelector('.picture__comments');
-  const likesPicture = infoPicturesTemplate.querySelector('.picture__likes');
+    // Находим в шаблоне picture нужные элементы
+    const imgPicture = infoPicturesTemplate.querySelector('.picture__img');
+    const commentsPicture = infoPicturesTemplate.querySelector('.picture__comments');
+    const likesPicture = infoPicturesTemplate.querySelector('.picture__likes');
 
-  // И пишем переданные значения
-  imgPicture.src = objectComment.url;
-  commentsPicture.textContent = objectComment.likes;
-  likesPicture.textContent = objectComment.comments.length;
+    imgPicture.src = comment.url;
+    commentsPicture.textContent = comment.comments.length;
+    likesPicture.textContent = comment.likes;
 
-  blockPastPictures.append(infoPicturesTemplate);
+    fragment.appendChild(infoPicturesTemplate);
+  });
+
+  blockPastPictures.appendChild(fragment);
 }
 
 export {createTemplate};

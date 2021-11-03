2151380f172f2da9854f18bcd694ec4532206ced
Grigoriy_Belikov <grisha.belikov@gmail.com>
2021-11-03 23:55:07 +0300

    - Фикс

--- js/full-size-image.js
+++ js/full-size-image.js
@@ -1,8 +1,26 @@
-// Функция добавляет класс hidden к классу
-//!!!!!!!!! Почему мы можем обьявить функцию тут, а можем в конце
-//!!!!!!!!! после экспорта и она все равно будет работать???
+// Функция делает класс невидимым
 function addHiddenTag(className) {
   className.classList.add('hidden');
+}
+
+function closeFullImage(bodyElement) {
+  const bigPictureSection = document.querySelector('.big-picture');
+  // Кнопка закрытия большой картинки
+  const contentPicture = document.querySelector('#picture-cancel');
+
+  // !!!!!!!!!! В функциях ниже повторяющийся код. Как обьединить?
+  // Закрываем по крестику или кнопке ESC фулл фото
+  document.addEventListener('keydown', (evt) => {
+    if (evt.keyCode === 27) { // 27 === ESC
+      addHiddenTag(bigPictureSection);
+      bodyElement.classList.remove('modal-open');
+    }
+  });
+
+  contentPicture.onclick = function() {
+    addHiddenTag(bigPictureSection);
+    bodyElement.classList.remove('modal-open');
+  };
 }
 
 function openFullImage () {
@@ -13,14 +31,9 @@ function openFullImage () {
   const commentsCount = document.querySelector('.comments-count');
   const socialComments = document.querySelector('.social__comments');
   const socialCaption = document.querySelector('.social__caption');
-  const socialCommentCounter = document.querySelector('.social__comment-count');
+  const socialCommentCount = document.querySelector('.social__comment-count');
   const commentsLoader = document.querySelector('.comments-loader');
   const bodyElement = document.querySelector('body');
-
-  // Кнопка закрытия большой картинки
-  const contentPicture = document.querySelector('#picture-cancel');
-
-  // const picturesArray = document.querySelectorAll('.picture');
 
   const thumbnails = document.querySelectorAll('.picture__img');
   const likes = document.querySelectorAll('.picture__likes');
@@ -35,46 +48,40 @@ function openFullImage () {
 
       // Добавляем ссыль из миниатюры в полную версию
       bigPictureImg.src = thumbnails[i].src;
+      bigPictureImg.alt = `Фотография в полном размере ${[i]}`;
+
       //!!!!!!! Добавляем описание в альт(или куда?? в задании ошибка -
       // поля дескрипшн нет в задании!) из миниатюры в полную версию
       socialCaption.textContent = thumbnails[i].alt;
-
       // Добавляем кол-во лайков
       likesCounter.textContent = likes[i].textContent;
-
       // Добавляем кол-во комментариев
       commentsCount.textContent = comments[i].textContent;
 
-      // прячем классы, добавляя им класс hidden
-      // с ними мы разберёмся позже, в другом домашнем задании
-      // commentsLoader(socialCommentCounter);
-      // addHiddenTag(commentsLoader);
+      // прячем классы social__comment-count и comments-loader, добавляя им
+      // класс hidden. С ними мы разберёмся позже, в другом домашнем задании
+      addHiddenTag(socialCommentCount);
+      addHiddenTag(commentsLoader);
 
       // Добавляем или убираем тегу body класс modal-open,
       // чтобы контейнер с фотографиями позади не прокручивался
-      // bodyElement.classList.add('modal-open');
-      // bodyElement.classList.remove('modal-open');
+      bodyElement.classList.add('modal-open');
 
-      document.addEventListener('keydown', function(evt) {
-        // Проверяем, что код клавиши равен 27 (ESC)
-        if (evt.keyCode === 27) {
-          console.log('ESC');
-          addHiddenTag(bigPictureSection);
-        }
-      });
-
-      contentPicture.onclick = function() {
-        console.log('Вы нажали на кнопку');
-        addHiddenTag(bigPictureSection);
-      };
+      closeFullImage(bodyElement);
 
       console.log(commentsCount.textContent);
+
+
+      // Удаляем содержимое social__comment
+      while (socialComments.firstChild) {
+        socialComments.removeChild(socialComments.lastChild);
+      }
+
+
+
+
     };
   }
-
-  // console.log('aqqaqaqaq');
-  // console.log(picturesArray[0]);
-
 }
 
 export {openFullImage};

fcc09ad72673f737f39f57def04c5607a5c1cb1f
Grigoriy_Belikov <grisha.belikov@gmail.com>
2021-11-18 01:19:45 +0300

    - Исправлено отображение комментариев для фильтров

--- js/paint-image.js
+++ js/paint-image.js
@@ -27,47 +27,53 @@ const addContent = (comment) => {
 // Функция рисует фотографии по умолчанию из массива
 const addContentToTemplateDefaultOrder = (commentsArray) => {
   // Пишем переданные значения
+  const newDefaultCommentsArray = [];
   commentsArray.forEach((comment) => {
     addContent(comment);
+    newDefaultCommentsArray.push(comment);
   });
   // Удаляем старые элементы
   document.querySelectorAll('.picture').forEach((element) => element.remove());
 
   blockPastPictures.appendChild(fragment);
-  setupFullImage(commentsArray);
+  setupFullImage(newDefaultCommentsArray);
 };
 
 // Функция сортирует фотографии по убыванию кол-ва комментариев в них
 const addContentToTemplateDescendingOrder = (commentsArray) => {
   // Пишем переданные значения
+  const newDescendingCommentsArray = [];
   commentsArray
     .slice()
     .sort((elemA, elemB) => elemB.comments.length - elemA.comments.length)
     .forEach((comment) => {
       addContent(comment);
+      newDescendingCommentsArray.push(comment);
     });
   // Удаляем старые элементы
   document.querySelectorAll('.picture').forEach((element) => element.remove());
 
   blockPastPictures.appendChild(fragment);
-  setupFullImage(commentsArray);
+  setupFullImage(newDescendingCommentsArray);
 };
 
 // Функция рисует AMOUNT_RANDOM_COMMENTS случайных фотографий
 const addContentToTemplateRandomOrder = (commentsArray) => {
   // Пишем переданные значения
+  const newRandomCommentsArray = [];
   commentsArray
     .slice()
     .sort(() => 0.5 - Math.random())
     .slice(0, AMOUNT_RANDOM_PHOTOS)
     .forEach((comment) => {
       addContent(comment);
+      newRandomCommentsArray.push(comment);
     });
   // Удаляем старые элементы
   document.querySelectorAll('.picture').forEach((element) => element.remove());
 
   blockPastPictures.appendChild(fragment);
-  setupFullImage(commentsArray);
+  setupFullImage(newRandomCommentsArray);
 };
 
 export {addContentToTemplateDefaultOrder};

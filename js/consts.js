19f47355dafcc0a25dac78d489a80dac3d0592b0
Grigoriy_Belikov <grisha.belikov@gmail.com>
2021-11-22 01:17:11 +0300

    - Б1. Код соответствует техническому заданию проекта.     - Поправил валидацию хэштегов.
      - Б2. При выполнении кода не возникает необработанных ошибок.
        - поправил: "Возникает ошибка при клике двух раз на кнопку "опубликовать" если у нас неверно заполнена форма".

--- js/consts.js
+++ js/consts.js
@@ -3,6 +3,7 @@ const MAX_AVALIABLE_HASHTAGS = 5;
 const AMOUNT_RANDOM_PHOTOS = 10;
 const ESC_KEYBUTTON = 27;  // 27 = ESC
 const MAX_STRING_LENGTH = 140;
+const MAX_LENGTH_HASHTAG = 20;
 const ALERT_SHOW_TIME = 10000;
 const RERENDER_DELAY = 500;
 const FILE_TYPES = ['jpg', 'jpeg', 'png'];
@@ -10,19 +11,19 @@ const URL_GET_DATA = 'https://24.javascript.pages.academy/kekstagram/data/';
 const URL_POST_DATA = 'https://24.javascript.pages.academy/kekstagram/';
 
 const ERROR_MESSAGES = {
-  errorMaxAmountHashtags:  'Нельзя указать больше пяти хэш-тегов',
-  errorRepetitiveHashtah:  'Имеются повторяющиеся хэштеги',
-  errorFirstSymbol:        'Хэштег должен начинаться с #',
-  errorMinSymbols:         'Хэштег не может состоять только из одной решётки',
-  errorMaxLengthHashtag:   'Максимальная длина одного хэштега 20 символов',
-  errorWrongSymbols:       'Хэштег может содержать только цифры, буквы и нижнее подчеркивание',
-  errorMessageLenComment:  'Длина комментария не может составлять больше 140 символов',
-  errorNoDataReceived:     'Не удалось получить данные с сервера',
-  errorFailedToSubmitForm: 'Не удалось отправить форму',
+  errorMaxAmountHashtags:  'Нельзя указать больше пяти хэштегов\n',
+  errorRepetitiveHashtah:  'Имеются повторяющиеся хэштеги\n',
+  errorFirstSymbol:        'Хэштег должен начинаться с #\n',
+  errorMinSymbols:         'Хэштег не должен состоять только из одной решётки\n',
+  errorMaxLengthHashtag:   'Максимальная длина одного хэштега 20 символов\n',
+  errorWrongSymbols:       'Хэштег может содержать только цифры, буквы и нижнее подчеркивание\n',
+  errorMessageLenComment:  'Длина комментария не может составлять больше 140 символов\n',
+  errorNoDataReceived:     'Не удалось получить данные с сервера\n',
+  errorFailedToSubmitForm: 'Не удалось отправить форму\n',
 };
 
 export {
   ERROR_MESSAGES, MAX_DISPLAYED_COMMENTS, ESC_KEYBUTTON, MAX_AVALIABLE_HASHTAGS,
-  MAX_STRING_LENGTH, ALERT_SHOW_TIME, URL_GET_DATA, URL_POST_DATA, FILE_TYPES,
-  AMOUNT_RANDOM_PHOTOS, RERENDER_DELAY
+  MAX_STRING_LENGTH, ALERT_SHOW_TIME, MAX_LENGTH_HASHTAG, URL_GET_DATA,
+  URL_POST_DATA, FILE_TYPES, AMOUNT_RANDOM_PHOTOS, RERENDER_DELAY
 };
